a44acd34e2ab0599a9b3c16da12825e55bf4a93e
jeanettepranin <jeanettepranin@gmail.com>
2017-04-15 12:31:29 -0500

    add skeleton code

--- spellchecker/src/main.rs
+++ spellchecker/src/main.rs
@@ -1,3 +1,38 @@
+extern crate regex;
+
+use regex::Regex;
+use std::io::{Read,BufReader,BufRead,stdout,Write,stdin};
+
 fn main() {
-    println!("Hello, world!");
+    let corpus = read_corpus();
+    let input = read_input(stdin());
+    write_output(stdout(), &corpus, &spellchecker(&input));
+}
+
+fn read_corpus() {
+
+}
+
+fn read_input<R: Read>(reader: R) -> Vec<String> {
+	let mut words: Vec<String> = vec![];
+	let mut lines = BufReader::new(reader).lines();
+	
+	let re = Regex::new(r"[^\P{P}-]+").unwrap();
+
+	while let Some(Ok(line)) = lines.next() {
+		//strip punctuation
+		let sw = re.replace_all(line.as_str(), "").to_string().replace("--", " ");
+		//split words between spaces
+		let sw_split = sw.split(" ");
+		for s in sw_split {
+			if !s.trim().is_empty() {
+				words.push(s.trim().to_lowercase());
+			}
+		}
+	}
+
+	words
+}
+
+fn write_output<W: Write>(mut writer: W, bmap: &BTreeMap<String, usize>) {
 }
