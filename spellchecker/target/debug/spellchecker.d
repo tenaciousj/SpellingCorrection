af546801200c20638cbcf6d925e87eca12ac7f21
jeanettepranin <jeanettepranin@gmail.com>
2017-04-15 13:44:08 -0500

    delete target

--- spellchecker/target/debug/spellchecker.d
+++
@@ -1,1 +0,0 @@
-/Users/jeanettepranin1/Desktop/EECS_395_Rust/SpellingCorrection/spellchecker/target/debug/spellchecker: /Users/jeanettepranin1/Desktop/EECS_395_Rust/SpellingCorrection/spellchecker/src/main.rs
