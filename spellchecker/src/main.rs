435a7c843f2db4fec8421a8f4102868a1de9cdef
kevin <kevinqjin@gmail.com>
2017-04-18 20:47:57 -0500

    stdin and corpus read work ~dab~

--- spellchecker/src/main.rs
+++ spellchecker/src/main.rs
@@ -33,105 +33,100 @@ impl PartialOrd for WordFreq {
 fn main() {
     // let args = env::args().collect::<String>();
     // let split_args = args.split_whitespace();
-    // for w in split_args {
-    // 	println!("{}", w);
-    	
-    // }
-    // let corpus = read_corpus(&args).unwrap();
-    // let input = read_input(stdin());
-
-    // for word in input {
-    // 	println!("{}", word);
-    // }
-    let tv = replace_edit(&"hello");
-    for t in tv {
-    	println!("{}", t);
+    let mut args = env::args();
+    if args.len() != 2 {
+    	println!("usage: spellchecker corpus.txt");
+    	return
     }
+    args.next();
+    let corpus_file = args.next().unwrap();
+    let corpus = read_corpus(&corpus_file).unwrap();
+    let input = read_input(stdin());
 }
 
-// fn read_corpus(filename: &str) -> std::io::Result<HashMap<String, usize>>{
-// 	let file = File::open(filename)?;
-// 	let mut hmap = HashMap::<String, usize>::new();
-// 	let mut buf_reader = BufReader::new(file);
-// 	let mut contents = String::new();
-// 	buf_reader.read_to_string(&mut contents)?;
-// 	let clean_contents = contents.trim().to_lowercase();
-// 	let split_contents = clean_contents.split_whitespace();
-// 	for c in split_contents {
-//   		let val = hmap.entry(c.to_string()).or_insert(0);
-//         // add 1 to value
-//         *val += 1;
-// 	}
-// 	Ok(hmap)
-// }
+fn read_corpus(filename: &str) -> std::io::Result<HashMap<String, usize>>{
+	let file = File::open(filename)?;
+	let mut hmap = HashMap::<String, usize>::new();
+	let mut buf_reader = BufReader::new(file);
+	let mut contents = String::new();
+	buf_reader.read_to_string(&mut contents)?;
+	let clean_contents = contents.trim().to_lowercase();
+	let split_contents = clean_contents.split_whitespace();
+	for c in split_contents {
+  		let val = hmap.entry(c.to_string()).or_insert(0);
+        // add 1 to value
+        *val += 1;
+	}
+	Ok(hmap)
+}
 
-// fn read_input<R: Read>(reader: R) -> Vec<String> {
-// 	let mut words = vec![];
-// 	let mut lines = BufReader::new(reader).lines();
+fn read_input<R: Read>(reader: R) -> Vec<String> {
+	let mut words = vec![];
+	let mut lines = BufReader::new(reader).lines();
 
-// 	while let Some(Ok(line)) = lines.next() {
-// 		let in_words = line.trim().to_lowercase();
-// 		let split_words = in_words.split_whitespace();
-// 		for w in split_words {
-// 			words.push(w.to_string());
-// 		}
-// 	}
-// 	words
-// }
+	while let Some(Ok(line)) = lines.next() {
+		let in_words = line.trim().to_lowercase();
+		let split_words = in_words.split_whitespace();
+		for w in split_words {
+			words.push(w.to_string());
+		}
+	}
+	words
+}
 
-// fn spell_check(vec_str: &Vec<String>, corpus: &HashMap<String, usize>) -> Vec<String> {
-// 	let mut output_vec = vec![];
-// 	for check_word in vec_str {
-// 		if corpus.contains_key(check_word) {
-// 			output_vec.push(check_word.to_string());
-// 		} else {
-// 			let mut one_edit_vec = create_variations(&check_word);
-// 			let mut wordfreq_vec = Vec::<WordFreq>::new();
-// 			for edited_word in one_edit_vec {
-// 				if corpus.contains_key(&edited_word) {
-// 					wordfreq_vec.push( WordFreq { freq: corpus[&edited_word], word: edited_word.clone()});
-// 				}
-// 				let mut two_edit_vec = create_variations(&edited_word);
-// 				for edited_word2 in two_edit_vec {
-// 					if corpus.contains_key(&edited_word2) {
-// 						wordfreq_vec.push( WordFreq { freq: corpus[&edited_word2], word: edited_word2});
-// 					}
-// 				}
-// 			}
-// 			let mut output_str = check_word.clone();
-// 			output_str.push_str(", ");
-// 			if wordfreq_vec.is_empty() {
-// 				output_str.push_str("-\n");
-// 			} else {
-// 				wordfreq_vec.sort();
-// 				output_str.push_str(&wordfreq_vec[0].word);
-// 				output_str.push_str("\n");
-// 			}
-// 			output_vec.push(output_str.to_string());
-// 		}
-// 	}
-// 	output_vec
-// }
+fn spell_check(vec_str: &Vec<String>, corpus: &HashMap<String, usize>) -> Vec<String> {
+	let mut output_vec = vec![];
+	for check_word in vec_str {
+		if corpus.contains_key(check_word) {
+			output_vec.push(check_word.to_string());
+		} else {
+			let mut one_edit_vec = create_variations(&check_word);
+			let mut wordfreq_vec = Vec::<WordFreq>::new();
+			for edited_word in one_edit_vec {
+				if corpus.contains_key(&edited_word) {
+					wordfreq_vec.push( WordFreq { freq: corpus[&edited_word], word: edited_word.clone()});
+				}
+				let mut two_edit_vec = create_variations(&edited_word);
+				for edited_word2 in two_edit_vec {
+					if corpus.contains_key(&edited_word2) {
+						wordfreq_vec.push( WordFreq { freq: corpus[&edited_word2], word: edited_word2});
+					}
+				}
+			}
+			let mut output_str = check_word.clone();
+			output_str.push_str(", ");
+			if wordfreq_vec.is_empty() {
+				output_str.push_str("-\n");
+			} else {
+				wordfreq_vec.sort();
+				output_str.push_str(&wordfreq_vec[0].word);
+				output_str.push_str("\n");
+			}
+			output_vec.push(output_str.to_string());
+		}
+	}
+	output_vec
+}
 
 
-// fn create_variations(word: &String) -> Vec<String> {
-// 	let mut vec_variations = vec![];
-// 	let mut temp_vec = delete_edit(&word);
-// 	vec_variations.append(&mut temp_vec);
-// 	temp_vec = transpose_edit(&word);
-// 	vec_variations.append(&mut temp_vec);
-// 	temp_vec = replace_edit(&word);
-// 	vec_variations.append(&mut temp_vec);
-// 	temp_vec = insert_edit(&word);
-// 	vec_variations.append(&mut temp_vec);
+fn create_variations(word: &String) -> Vec<String> {
+	let mut vec_variations = vec![];
+	let mut temp_vec = delete_edit(&word);
+	vec_variations.append(&mut temp_vec);
+	temp_vec = transpose_edit(&word);
+	vec_variations.append(&mut temp_vec);
+	temp_vec = replace_edit(&word);
+	vec_variations.append(&mut temp_vec);
+	temp_vec = insert_edit(&word);
+	vec_variations.append(&mut temp_vec);
 
-// 	// vec_variations.append(delete_edit(&word));
-// 	// vec_variations.append(transpose_edit(&word));
-// 	// vec_variations.append(replace_edit(&word));
-// 	// vec_variations.append(insert_edit(&word));
+	// vec_variations.append(delete_edit(&word));
+	// vec_variations.append(transpose_edit(&word));
+	// vec_variations.append(replace_edit(&word));
+	// vec_variations.append(insert_edit(&word));
 
-// 	vec_variations
-// }
+	vec_variations
+}
 
 fn delete_edit(word: &str) -> Vec<String> {
 	let mut output_vec = vec![];
@@ -178,9 +173,9 @@ fn insert_edit(word: &String) -> Vec<String> {
 }
 
 
-// fn write_output<W: Write>(mut writer: W, vec: &Vec<String> ) {
-// 	for word in vec {
-// 		writeln!(writer, "{}", word).unwrap();
-// 	}
+fn write_output<W: Write>(mut writer: W, vec: &Vec<String> ) {
+	for word in vec {
+		writeln!(writer, "{}", word).unwrap();
+	}
 	
-// }
+}
